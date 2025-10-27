c2413954b6f5e99a29b8af32faabb07073db6946
RinaldoEB <rinaldoedbm@gmail.com>
2025-10-27 22:06:53 +0700

    menambahkkan efek react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ColorBends from './components/ColorBends';
+import SplitText from "./components/SplitText";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleAnimationComplete = () => {
+  console.log('All letters have animated!');
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    <div className='grid place-items-center bg-stone-900 relative h-full'>
+      <SplitText
+        text="Hello Aldo Ganteng!"
+        className="text-5xl grid justify-items-center font-semibold text-center absolute z-100 text-white pointer-events-none"
+        delay={100}
+        duration={0.6}
+        ease="power3.out"
+        splitType="chars"
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+        rootMargin="-100px"
+        textAlign="center"
+        onLetterAnimationComplete={handleAnimationComplete}
+      />
+      <ColorBends
+        // style={"height :100px"}
+        colors={["#ff5c7a", "#8a5cff", "#00ffd1"]}
+        rotation={10}
+        speed={0.7}
+        scale={1.5}
+        frequency={2}
+        warpStrength={1.2}
+        mouseInfluence={0.8}
+        parallax={0.6}
+        noise={0.08}
+        transparent={false}
+      />
+    </div>
+  
+  );
 }
 
-export default App
+export default App;
